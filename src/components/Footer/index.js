a13a7913ff55ee84c285011a77d9bf4f1b3902ee
Kama <kama@wietrzyk.com>
2021-02-08 14:37:02 +0100

    Corrections

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -13,9 +13,15 @@ const Footer = ({ location }) => {
                 <img src={footerPic} alt="konsultantki ślubne z agencji ślubnej Czary Marry - organizatorki ślubów i wesel" />
                 <div className="Footer-box_text">
                     <h2>Napisz <span>do nas!</span></h2>
-                    <p>kontakt@czarymarry.com</p>
-                    <p>Kasia 577-900-036,<span className="mobile"><br/></span> Ola 726-594-267</p>
-                    <p className="bottom">Agencja ślubna Czary Marry <br/><Link to={generatePath(URLS.LANDING, { path: 'organizacja-wesel-wroclaw'})} className="CustomLink text-white">Wrocław</Link>, <Link to={generatePath(URLS.LANDING, { path: 'wedding-planner-poznan'})} className="CustomLink text-white">Poznań</Link> </p>
+                    {/* <p></p> */}
+                    <span className="bottom">
+                        <p className="mb-1">kontakt@czarymarry.com</p>
+                        <p>Kasia 577-900-036,<span className="mobile"><br /></span> Ola 726-594-267</p>
+                        <p className="mt-4">Agencja ślubna Czary Marry</p>
+                        <p>
+                            <Link to={generatePath(URLS.LANDING, { path: 'organizacja-wesel-wroclaw' })} className="CustomLink text-white">Wrocław</Link>, <Link to={generatePath(URLS.LANDING, { path: 'wedding-planner-poznan' })} className="CustomLink text-white">Poznań</Link>
+                        </p>
+                    </span>
                 </div>
             </div>
             <div className="Footer-contact">
