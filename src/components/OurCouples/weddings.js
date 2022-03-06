c586453192a004d4dc0e005570af50e8fd443545
Kama <kama@wietrzyk.com>
2022-03-06 19:47:48 +0100

    Patrycja i Przemek - realizacja

--- src/components/OurCouples/weddings.js
+++ src/components/OurCouples/weddings.js
@@ -678,47 +678,189 @@ const weddings = [
     },
     {
         bgColor: "bgDark",
-        title: "Angelika & Paweł",
-        details: "Zamek Książ zachwyca niesamowitą architekturą i historią. Dzięki zaufaniu i otwartości pary oraz niezastąpionej ekipie udało nam się dopisać kolejny, cudowny rozdział tej historii i stworzyć niesamowity klimat.",
+        title: "Patrycja i Przemek - wesele w Ceglarni Jarosławki",
+        path: "ceglarnia-jaroslawki-wesele",
+        minHeight: true,
+        seoTitle: "Ceglarnia Jarosławki - wesele w wykonaniu Czary Marry",
+        seoText: "Planujesz zorganizować swoje wesele w  Ceglarni Jarosławki? Zobacz realizację agencji ślubnej Czary Marry!",
+        mainPic: {
+            src: require('./OurCouplesPics/PatrycjaPrzemek/mainPic.jpg').default
+        },
+        details: "Byłyśmy niezwykle szczęśliwe, kiedy Patrycja i Przemek zaprosili nas do współpracy w tym niesamowitym miejscu. Ciężko sobie wyobrazić, że nie tak dawno były to wyłącznie niszczejące budynki starego PGR-u.",
         org: "Agencja Ślubna Czary Marry",
         orgLink: "",
-        place: "Zamek Książ",
-        placeLink: "https://www.ksiaz.walbrzych.pl/",
-        photo: "Jam Studio",
-        photoLink: "https://jamstudio.pl/",
-        photoTwo: "Magdalena Adamczewska",
-        photoLinkTwo: "https://www.facebook.com/magdalena.adamczewska",
-        movie: "Cameralni",
-        movieLink: "http://cameralni.com/",
-        flowers: "ArtSize",
-        flowersLink: "https://www.artsize.pl/",
-        photos: {
-            pic1: require('./OurCouplesPics/AngelikaPawel/01.jpg').default,
-            pic2: require('./OurCouplesPics/AngelikaPawel/02.jpg').default,
-            pic3: require('./OurCouplesPics/AngelikaPawel/03.jpg').default,
-            pic4: require('./OurCouplesPics/AngelikaPawel/04.jpg').default,
-            pic5: require('./OurCouplesPics/AngelikaPawel/05.jpg').default,
-            pic6: require('./OurCouplesPics/AngelikaPawel/06.jpg').default,
-            pic7: require('./OurCouplesPics/AngelikaPawel/07.jpg').default,
-            pic8: require('./OurCouplesPics/AngelikaPawel/08.jpg').default,
-            pic9: require('./OurCouplesPics/AngelikaPawel/09.jpg').default,
-            pic10: require('./OurCouplesPics/AngelikaPawel/11.jpg').default,
-            pic11: require('./OurCouplesPics/AngelikaPawel/12.jpg').default,
-            pic12: require('./OurCouplesPics/AngelikaPawel/10.jpg').default,
-            pic16: require('./OurCouplesPics/AngelikaPawel/16.jpg').default,
-            pic17: require('./OurCouplesPics/AngelikaPawel/17.jpg').default,
-            pic18: require('./OurCouplesPics/AngelikaPawel/18.jpg').default,
-            pic19: require('./OurCouplesPics/AngelikaPawel/19.jpg').default,
-            picVer1: require('./OurCouplesPics/AngelikaPawel/ver2.jpg').default,
-            picVer2: require('./OurCouplesPics/AngelikaPawel/ver4.jpg').default,
-            picVer3: require('./OurCouplesPics/AngelikaPawel/ver3.jpg').default,
-            picVer6: require('./OurCouplesPics/AngelikaPawel/ver6.jpg').default,
-            picVer7: require('./OurCouplesPics/AngelikaPawel/ver7.jpg').default,
-            classNameOne: "twoVertHorizFlipped",
-            classNameTwo: "threeHoriz",
-            classNameThree: "twoVertHoriz",
-            classNameFive: "threeHorizFlipped"
-        }
+        place: "Ceglarnia Jarosławki",
+        placeLink: "https://www.ceglarnia.com/",
+        photo: "Jakub Nowotyński",
+        photoLink: "https://fotoreportazslubny.com/",
+        flowers: "Anello Decor",
+        flowersLink: "https://www.facebook.com/anellodecor/",
+        date: "06 mar 2022",
+        textMain: <>Będziemy z Wami szczere - zarówno my, jak i większość branży ślubnej uwielbia wracać do <span className="bold mint">Ceglarni Jarosławki</span>. Byłyśmy niezwykle szczęśliwe, kiedy Patrycja i Przemek zaprosili nas do współpracy w tym niesamowitym miejscu. Odwiedzałyśmy ją wiele razy przy innych okazjach, ale jakoś nie było sposobności, żeby zorganizować tam wesele. Ceglarnia to nie tylko piękna stodoła, kochamy ją również za uniwersalność, otoczenie, kompleksowość i miłą atmosferę. Ciężko sobie wyobrazić, że nie tak dawno były to wyłącznie niszczejące budynki starego PGR-u.</>,
+        topPic: {
+            src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-cena-wesela-01.jpg').default
+        },
+        content: [
+            {
+                headerSmall: "Ceglarnia Jarosławki — piękne miejsce pośrodku niczego.".toUpperCase(),
+                headerClassName: "dark",
+                text: "Ceglarnia przez swoje położenie sprawia wrażenie bardzo tajemniczego miejsca. Jadąc tam po raz pierwszy, podekscytowanie wzrasta z każdą chwilą, nasuwa się myśl: czy na pewno dobrze jedziemy? Jeszcze do niedawna prowadziła tam dziurawa, szutrowa droga, o której w branży ślubnej krążyły legendy. Dziś dojedziecie tam już nowiutką asfaltówką. Lokalizacja Ceglarnii, jej oddalenie od zabudowań i duży teren dookoła czyni to miejsce niezwykle kameralnym i intymnym.",
+                pics: [
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-cena-wesela-02.jpg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-cena-wesela-03.jpg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-cena-wesela-04.jpg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-cena-wesela-05.jpg').default
+                    },
+                ]
+            },
+            {
+                headerSmall: "Ceglarnia Jarosławki — kompleksowość, o której marzy każda para młoda.".toUpperCase(),
+                headerClassName: "dark",
+                text: <>Mało jest miejsc na weselnej mapie polski, które świadczyłyby tak kompleksową obsługę. W zasadzie <CustomOuterLink text="Ceglarnia" to="https://www.ceglarnia.com/" className="linkMint" /> jest samowystarczalna pod każdym względem.</>,
+                pics: [
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-jaroslawki-cena-wesele-01.jpg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-jaroslawki-cena-wesele-02.jpg').default
+                    },
+                ]
+            },
+            {
+                headerSmall: "Otoczenie Ceglarni Jarosławki.",
+                headerClassName: "mint",
+                text: <>To 5h terenu, na które składa się plac przed salą z pięknie wystrzyżonym trawnikiem, sad, park oraz czego dusza zapragnie, żeby zorganizować ceremonię cywilną w plenerze. Konkretne miejsce możecie wybrać sami. W razie złej pogody nie musicie się martwić. Stodoły w Ceglarni Jarosławki są dwie. Tą mniejszą możecie wykorzystać jako miejsce ceremonii w razie opadów deszczu. Ponadto 11 kilometrów od sali, w miejscowości Gogolewo znajduje się zabytkowy, drewniany kościółek. Teren wokół sali daje ogromne możliwości fotografowi ślubnemu, który może poszaleć z parą młodą na sesji, nie opuszczając terenu Ceglarni. U nas za obiektywem stanął nieoceniony <CustomOuterLink text="Jakub Nowotyński" to="https://fotoreportazslubny.com" className="linkMint" />.</>,
+                pics: [
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-jaroslawki-cena-wesele-03.jpg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-jaroslawki-cena-wesele-04.jpg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-jaroslawki-cena-wesele-05.jpg').default
+                    },
+                ]
+            },
+            {
+                headerSmall: "Baza noclegowa.",
+                headerClassName: "mint",
+                text: "90 miejsc noclegowych w 2-,3- lub 4-osobowych pokojach. To jeszcze nie koniec, bo właściciel dzielnie pracuje nad powiększeniem tej bazy. Pokoje są utrzymane w industrialnym, a jednocześnie przytulnym klimacie. Mają bardzo dobre ceny, znajdują się zaraz przy sali, ale w odrębnym budynku. Ci, którzy chcieliby wcześniej pójść spać, nie będą słyszeć głośnej muzyki. To nie wszystko, na uwagę zasługuje Pałacyk, po drugiej stronie parku, gdzie para młoda może w kameralnym gronie przygotowywać się do wesela. Przeszklone pokoje, niesamowita przestrzeń wspólna i charakterystyczne czerwone drzwi. Zdarza się, że goście decydują się na przyjazd dzień wcześniej, żeby przedłużyć sobie weselny weekend.",
+                pics: [
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-jaroslawki-opinie-01.jpg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-jaroslawki-opinie-02.jpg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-jaroslawki-opinie-03.jpg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-jaroslawki-opinie-04.jpg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-jaroslawki-opinie-05.jpg').default
+                    },
+                ]
+            },
+            {
+                headerSmall: "Catering.",
+                headerClassName: "mint",
+                text: <>Wielu przeraża zatrudnienie zewnętrznego cateringu, ale to świetne rozwiązanie, kiedy współpracujesz z odpowiednią i wykwalifikowaną ekipą. Zresztą właściciel lub manager pomoże Wam przejść przez ten proces. W zasadzie nie musicie robić nic więcej niż przy wyborze miejsca z własną kuchnią. Cegielnia Jarosławki współpracuje m.in. z cateringiem Modra. Potrawy przygotowywane są z sezonowych składników, a menu inspirowane tradycją ludową Polski i Europy. Jedzenie jest naprawdę smaczne. Jeśli nie wierzycie, koniecznie odwiedźcie <CustomOuterLink text="Restaurację Modra Kuchnia" to="https://www.facebook.com/ModraKuchnia/" asFB className="linkMint" /> w Poznaniu. W Ceglarnii zjecie też pyszne kontynentalne śniadanie po weselu lub zorganizujcie poprawinowego grilla.</>,
+                pics: [
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-wesele-01.jpg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-wesele-02.jpg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-wesele-03.jpg').default
+                    },
+                ]
+            },
+            {
+                headerSmall: "Wynajem dekoracji.",
+                headerClassName: "mint",
+                text: <>Magazyn Ceglarnii Jarosławki pęka w szwach. Znajdziecie tam wiele ciekawych wazonów, świeczników, skrzynek, starą szafę, proporczyki oraz oświetlenie sufitowe, a nawet drzewka na wynajem. Do tego florystka z Ceglarnii może przygotować dla Was dekoracje kwiatowe. Za te ze zdjęć odpowiada <CustomOuterLink text="Anello Decor" to="https://www.facebook.com/anellodecor/" asFB className="linkMint" />, jednak właściciel Ceglarni niczego Wam nie narzuca. Płacąc za wynajem przestrzeni możecie w niej zrobić to, czego dusza zapragnie. Oczywiście, jeżeli pozostawicie miejsce w nienaruszonym stanie.</>,
+                pics: [
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-wesele-04.jpeg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-wesele-05.jpeg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-wesele-06.jpeg').default
+                    },
+                ]
+            },
+            {
+                headerSmall: "Rozrywka i chill out.",
+                headerClassName: "mint",
+                text: "Poza tym, że możecie poleniuchować na leżakach przed salą lub pospacerować, na terenie Ceglarni Jarosławki znajdziecie wspólną przestrzeń z wygodnymi kanapami i stołem bilardowym. To może być świetne miejsce na przygotowania dla pana młodego, jeśli para zdecydowała, że nie chce się widzieć przed ślubem.",
+                pics: [
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-wesele-07.jpg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-wesele-08.jpg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-wesele-09.jpg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-wesele-10.jpg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-wesele-11.jpg').default
+                    },
+                ]
+            },
+            {
+                headerSmall: "Ceglarnia — wesele.".toUpperCase(),
+                headerClassName: "dark",
+                text: "Było już o cateringu, noclegach, miejscach na ceremonię i przygotowaniach. To bardzo ważne elementy, ale o wyborze miejsca decyduje głównie wygląd sali. Stara stodoła z zewnątrz wygląda niepozornie. Tym bardziej zaskakuje jej wnętrze. Niby industrialne, a jednak przyjazne, niby na 250 osób, a jednak nie świeci pustką, gdy zorganizujemy tam wesele na 90 os. Drewno, bielone ściany, betonowa posadzka i piękne stoły z krzesłami przypominającymi nieco te szkolne z dawnych lat. Mimo że sala jest duża i strzelista nie wymaga wcale ogromnych nakładów dekoracji. W zasadzie sama się broni. Na dodatek jest klimatyzowana, co w przypadku stodół nie jest zbyt powszechne.",
+                pics: [
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-wesele-12.jpg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-wesele-13.jpg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-wesele-14.jpg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-wesele-15.jpg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-wesele-16.jpg').default
+                    },
+                    {
+                        src: require('./OurCouplesPics/PatrycjaPrzemek/ceglarnia-wesele-17.jpg').default
+                    },
+                ]
+            },
+            {
+                text: <>Jeśli jesteście zainteresowani organizacją wesela w Ceglarnii Jarosławki lub innym miejscu na terenie <Link to={generatePath(URLS.LANDING, { path: 'wedding-planner-poznan' })} className="linkMint">województwa wielkopolskiego</Link> zapraszamy do kontaktu. Mamy niezwykłą słabość do tych terenów i bardzo lubimy tam wracać. Jeśli marzy Wam się przyjęcie w równie klimatycznym miejscu, ale do Jarosławek trochę Wam za daleko piszcie śmiało, na pewno coś na to poradzimy.</>
+            }
+        ],
+        extras: <>Może zainteresować Cię również:
+            <ul style={{ marginTop: ".5rem" }}>
+                <li><CustomPostLink text="Folwark Wąsowo - miejsce na wesele dające nieograniczone możliwości" path="folwark-wasowo-wesele" className="linkMint" /></li>
+                <li>Załatw wszystkie <CustomPostLink text="formalności przed ślubem" path="przygotowania-do-slubu-formalnosci" className="linkMint" /></li>
+                <li>Zorganizuj <CustomPostLink text="wesele w ogrodzie" path="wesele-w-ogrodzie-garden-party" className="linkMint" /></li>
+            </ul>
+        </>
     },
     {
         bgColor: "bgPink",
