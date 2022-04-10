c7926ae1403e39fecb24a6503146ab6a06512682
Kama <kama@wietrzyk.com>
2022-04-10 22:10:33 +0200

    MartaMichał - realizacja

--- src/components/OurCouples/weddings.js
+++ src/components/OurCouples/weddings.js
@@ -17,7 +17,244 @@ import { main as MagdaSzymonMain, jakiKolSuk, jesDekoKość, jesWes01, jesWes02,
 
 import { mainPic as AniaMateuszMain, urSiedSt01, urSiedSt02, urSiedSt03, urSiedSt04, urSiedSt05, urSiedSt06, urSiedSt07, urSiedSt08, urSiedSt09, urSiedSt10, urSiedSt11, urSiedSt12, urSiedSt13, urSiedSt14, urSiedSt15, urSiedStGola01, urSiedStGola02, urSiedStGola03, urSiedStLux01, urSiedStLux02, urSiedStLux00, urSiedStOp01, urSiedStOp02, urSiedStOp03, urSiedStWesCena01, urSiedStWesCena02, urSiedStWesCena03, urSiedStWesOp01, urSiedStWesOp02, urSiedStWesOp03, urSiedStWesOp04, urSiedStWesOp05 } from "./OurCouplesPics/AniaMateusz/index"
 
+import { main as MartaMichalMain, ślCywZim01, ślCywZim02, ślCywZim03, ślCywZim04, ślZima01, ślZima02, ślZima03, ślZimaOkr01, ślZimaOkr02, ślZimaOkr03, ślZimaOkr04, ślZimaOkr05, ślZimaOkr06, sukNaWesZima01, sukNaWesZima02, zimWes01, zimWes02, zimWes03, zimWes04, zimWes05, zimWes06, zimWes07, zimWes08, zimWes09, zimWes10, zimWes11, zimWes12, zimWes13, zimWes14, zimWes15, motPrzewWesZim } from "./OurCouplesPics/MartaMichal/index"
+
 const weddings = [
+    {
+        bgColor: "bgGrey",
+        title: "Zimowe wesele Marty i Michała",
+        path: "slub-zimowe-wesele",
+        minHeight: true,
+        seoTitle: "Zimowy ślub - wesele w stylu zimowym | Czary Marry agencja ślubna",
+        seoText: "Planujesz zorganizować zimowy ślub i przyjęcie weselne? Zobacz jak zimowe wesele wyczarowała agencja ślubna Czary Marry.",
+        mainPic: {
+            src: MartaMichalMain
+        },
+        details: 'Martę poznałyśmy na weselu jednej z naszych par młodych. Gdy się do nas odezwała, od razu wiedziałyśmy, że wspólnie wyczarujemy coś niesamowitego i niebanalnego. Gdy dowiedziałyśmy się, że będzie to zimowy ślub, nasze podekscytowanie sięgnęło zenitu.',
+        org: "Agencja Ślubna Czary Marry",
+        orgLink: "",
+        place: "Uroczysko Siedmiu Stawów",
+        placeLink: "https://www.uroczysko7stawow.pl/",
+        photo: "PieczkoPietras",
+        photoLink: "https://pieczkopietras.pixieset.com/",
+        movie: "Kosmos kosmos",
+        movieLink: "https://www.facebook.com/kosmosfilms",
+        flowers: "Art Size",
+        flowersLink: "https://www.artsize.pl/",
+        date: "10 kwi 2022",
+        textMain: "Martę poznałyśmy na weselu jednej z naszych par młodych. Gdy się do nas odezwała, od razu wiedziałyśmy, że wspólnie wyczarujemy coś niesamowitego i niebanalnego. Gdy dowiedziałyśmy się, że będzie to zimowy ślub, nasze podekscytowanie sięgnęło zenitu. Kochamy zimowy klimat i możliwość wyjścia poza schematy „typowego” wesela. Wbrew pozorom zimowe wesele to nie tylko świerk, szyszki i lampki choinkowe. Podobnie jak w wypadku wesel, które odbywają się w szczycie sezonu ślubnego mamy całą gamę możliwości. Chciałybyśmy podzielić się z Wami inspiracjami z przepięknego zimowego wesela w stylu krainy lodu. Mamy nadzieję, że ten pomysł spodoba Wam się równie mocno, jak nam.",
+        content: [
+            {
+                headerSmall: "Zimowe wesele - plusy:",
+                headerClassName: "dark",
+                text: <ol style={{ marginTop: ".5rem" }}>
+                    <li>Większa dostępność miejsc weselnych oraz wykonawców ślubnych.</li>
+                    <li>Lepsza frekwencja wśród gości — ślub zimą nie koliduje z wakacyjnymi planami.</li>
+                    <li>Niepowtarzalny klimat — możliwość zaskoczenia gości weselnych.</li>
+                    <li>Częste rabaty cenowe.</li>
+                </ol>,
+                pics: [
+                    {
+                        src: ślCywZim01
+                    },
+                    {
+                        src: ślCywZim02
+                    },
+                ]
+            },
+            {
+                headerSmall: "Ślub zimą — ceremonia kościelna.",
+                headerClassName: "dark",
+                text: <>Bez względu na to, na jaki rodzaj ceremonii się zdecydujecie, nie powinniście rezygnować z marzeń o pięknym zimowym ślubie. Ważne jednak by się odpowiednio przygotować. W przypadku <CustomPostLink text="ślubu kościelnego" path="dokumenty-do-slubu-koscielnego-konkordatowego" className="linkLight" /> pamiętajcie, że w świątyni może być chłodno, zwróćcie też uwagę, czy data nie zahacza o adwent. Zgodnie z prawem kościelnym możecie zorganizować wtedy wesele, ale zdarzają się księża starej daty, którzy kierują się własnymi przekonaniami. Jeśli Wasz zimowy ślub odbywa się w okolicach świąt, pamiętajcie, że w kościół może być udekorowany. To może być zarówno plus, jak i minus zimowego wesela. Jeżeli wokół ołtarza będą ustawione strzeliste choinki ozdobione światełkami, możecie się zaliczyć do grona szczęściarzy. Może się jednak zdarzyć, że będziecie musieli składać sobie przysięgę na tle stajenki, dzieląc przestrzeń z plastikowym dzieciątkiem w żłobku i osiołkiem. Tego typu dekoracja na ślub zimą nie jest najlepszym wyborem.</>,
+                pics: [
+                    {
+                        src: ślCywZim03
+                    },
+                    {
+                        src: ślCywZim04
+                    },
+                ]
+            },
+            {
+                headerSmall: "Ślub zimą — ceremonia cywilna.",
+                headerClassName: "dark",
+                text: <>Jak to wygląda w przypadku <CustomPostLink text="ślubu cywilnego" path="dokumenty-slub-cywilny-formalnosci" className="linkLight" /> zimą? Urzędnik może go Wam udzielić w pomieszczeniu sąsiadującym z salą. Pamiętajcie, że musi to być miejsce godne. Często zdarza się, że urzędnicy kręcą nosem na salę, w której później odbywa się konsumpcja. Ceremonia może się również odbyć w urzędzie lub jak w przypadku Marty i Michała możecie się zdecydować na ślub w plenerze. Zimą również da się to zorganizować. Oczywiście wymaga to wcześniejszego poinformowania urzędnika i jego dobrej woli. Czy jest zimno? Tak, ale można pomyśleć o nagrzewnicach, a ceremonia zajmuje pięć do dziesięciu minut. Podobnie będzie w przypadku ślubu humanistycznego. Radzimy jednak nie decydować się na długą ceremonię, ponieważ nie będzie to komfortowe ani dla Was, ani dla gości.<br />
+                    Problemem może się okazać również muzyka na żywo - nam udało się ją zorganizować, ale większość muzyków odmawia grania w niskiej temperaturze oraz przy wysokiej wilgotności powietrza, ze względu na obawę przed zniszczeniem instrumentów.</>,
+                pics: [
+                    {
+                        src: ślZima01
+                    },
+                    {
+                        src: ślZima02
+                    },
+                ]
+            },
+            {
+                headerSmall: "Motyw przewodni wesela zimą.",
+                headerClassName: "dark",
+                text: <> Najczęściej <CustomPostLink text="motywem przewodnim" path="motyw-przewodni-wesela" className="linkLight" /> jest już sama pora roku, ale nic nie stoi na przeszkodzie, żeby sięgnąć po zupełnie inne inspiracje, organizując wesele zimą. Dekoracje przede wszystkim mają być zgodne z Waszym wyobrażeniem i oczekiwaniami. Pamiętajcie, że kwiaty zimą potrafią być znacznie droższe, a ich dostępność i różnorodność jest mniejsza niż o innych porach roku. Najczęstszy motyw przewodni wesela zimą, na jaki decydują się pary młode, bazuje na igliwiu, zieleni, lesie i ciepłym świetle. Nic dziwnego, tworzą bardzo przytulny klimat i dobrze się kojarzą. </>,
+                pics: [
+                    {
+                        src: ślZima03
+                    },
+                    {
+                        src: motPrzewWesZim
+                    },
+                ]
+            },
+            {
+                headerSmall: "Wesele zimą — inspiracje od Czary Marry.".toUpperCase(),
+                headerClassName: "dark",
+                text: <>Razem z Martą postanowiłyśmy postawić na coś zupełnie innego. Z firmą <CustomOuterLink text="Art Size" to="https://www.artsize.pl" className="linkLight" /> wyczarowałyśmy lodową krainę, prawdziwe winter wedding.Postawiliśmy na przygaszone błękity, transparentność, bielone i farbowane rośliny.Ilość świeżych kwiatów na sali była minimalna.Na stołach stanęły bardzo wysokie kompozycje i piękne szklane kandelabry.Z gałęzi drzew spadały drobne koraliki, perły, i sople lodu.Zadbaliśmy o najmniejsze detale.Podtalerze, pierścienie na serwetki i krzesła strukturą miały przypominać lód.W dekoracjach pojawiły się również elementy lustrzane i srebro, które odbijało światło oraz dodawało elegancji.Zbudowaliśmy ściankę za parą młodą, której tłem był materiał z nadrukiem zimowej scenerii.Zadbaliśmy również o odpowiednie oświetlenie architektoniczne od nieocenionego teamu <CustomOuterLink text="BTL" to="https://www.btl-group.pl" className="linkLight" />. Całość uzupełnia zimowa aura panująca za oknem. Wszystko wyglądało bardzo elegancko i mroźnie. Zarówno para młoda, jak i goście nie mogli wyjść z podziwu. Dzięki pracy całego zespołu udało się stworzyć bajkowy ślub zimową porą, którego nie powstydziłby się sam Walt Disney. </>,
+                pics: [
+                    {
+                        src: zimWes01
+                    },
+                    {
+                        src: zimWes02
+                    },
+                    {
+                        src: zimWes03
+                    }
+                ]
+            },
+            {
+                headerSmall: "Zimowe wesele — zaproszenia na ślub.",
+                headerClassName: "grey",
+                text: "Zimowe zaproszenia, czyli jakie? Wszystko zależy od tego, na jakie dekoracje i kolory ostatecznie się zdecydujecie. Jeśli organizujecie zimowe wesele w góralskiej karczmie z kuligiem, nie bójcie się nawiązać do tego klimatu już na etapie zaproszeń. To piękna zapowiedź tego, co będzie się działo na weselu. W naszym przypadku wybór był oczywisty, postawiliśmy na transparentność. Zaproszenia były wydrukowane na pleksi i przewiązane delikatną wstążką. Wyglądały pięknie i elegancko. Postawiliśmy na ozdobną, białą czcionkę i delikatny motyw roślinny przypominający kwiaty malowane na szybach przez mróz. ",
+                pics: [
+                    {
+                        src: sukNaWesZima01
+                    },
+                    {
+                        src: sukNaWesZima02
+                    },
+                ]
+            },
+            {
+                headerSmall: "Sukienka na wesele zimą.",
+                headerClassName: "grey",
+                text: "Marta postawiła na piękną śnieżnobiałą suknię o klasycznym, ale eleganckim kroju. Materiał sprawiał wrażenie grubszego i mięsistego, był to bardzo trafny wybór. Najważniejsze jednak, żeby suknia była spełnieniem Twoich marzeń. Nie ma znaczenia, czy wybierzesz sukienkę mini z odkrytymi plecami, czy mocno zabudowaną z długim rękawem. Najważniejsze, żebyś czuła się w niej komfortowo i pięknie.",
+                pics: [
+                    {
+                        src: ślZimaOkr01
+                    },
+                ]
+            },
+            {
+                headerSmall: "Ślub zimą - okrycia.",
+                headerClassName: "grey",
+                text: <>To, co istotne to dodatkowe okrycie wierzchnie. W przypadku bardziej eleganckich stylizacji może to być wełniana lub kaszmirowa etola, sweterek, lub kożuszek. Przy luźniejszych stylizacjach świetnie sprawdzą się na przykład <CustomOuterLink to="https://pl.maxmara.com/p-8486011206001-ofelia-white" text="białe pikowane kurtki" className="linkLight" />.</>,
+                pics: [
+                    {
+                        src: ślZimaOkr02
+                    },
+                    {
+                        src: ślZimaOkr03
+                    },
+                ]
+            },
+            {
+                headerSmall: "Jakie buty do ślubu zimą.",
+                headerClassName: "grey",
+                text: "Wygodne i ciepłe. Jeśli marzą Ci się sandały to tylko podczas zabawy na parkiecie. Zadbaj o swój komfort i nie poświęcaj się bez sensu. Szkoda, żebyś odchorowała po weselu nieodpowiednie obuwie. Pamiętaj również, że to kwestia bezpieczeństwa, może być ślisko. Postaw na białe lub beżowe botki, a jeżeli jesteś nieco bardziej odważna, świetnie sprawdzą się również śniegowce.",
+                pics: [
+                    {
+                        src: ślZimaOkr04
+                    },
+                ]
+            },
+            {
+                headerSmall: "Jak się ubrać na wesele zimą — stylizacja Pana Młodego.",
+                headerClassName: "grey",
+                text: "U panów wygląda to nieco prościej niż w przypadku pań. Zachęcamy do skorzystania z okazji, jaką daje niższa temperatura i wyboru kamizelki. Warto zaszaleć również ze strukturą i zdecydować się na nieco grubszy materiał. Najlepiej postawić na nieco ciemniejsze kolory lub kratę, jasne beże i popiele radzimy zostawić na letnie stylizacje. W przypadku bardziej eleganckich przyjęć świetnie sprawdzi się klasyczny, ale koniecznie dobrze skrojony, czarny garnitur. Inne kolory, które warto rozważyć to granat, butelkowa zieleń, czy bordo.",
+                pics: [
+                    {
+                        src: MartaMichalMain
+                    },
+                    {
+                        src: ślZimaOkr05
+                    },
+                    {
+                        src: ślZimaOkr06
+                    },
+                ]
+            },
+            {
+                headerSmall: "Ślub w zimie — zdjęcia.",
+                headerClassName: "dark",
+                text: "Zdjęcia plenerowe w zimowej scenerii potrafią zachwycać. Musicie jednak pamiętać o kilku ważnych rzeczach. Pozowanie w takich warunkach wymaga poświęceń zarówno ze strony fotografa, jak i pary młodej. Jeśli jesteście gotowi na przedzieranie się przez zaspy, bitwę na śnieżki i mróz, który szczypie w nos i uszy możecie liczyć na niesamowite i spontaniczne ujęcia. Warto również zwrócić uwagę, że zimą bardzo szybko zachodzi słońce. Warto zaplanować dłuższą sesję innego dnia lub podczas first looku przed ceremonią. Zdjęcia wieczorem po zachodzie słońca również mają klimat, ale fajnie byłoby uchwycić również piękno otaczającej Was zimowej aury w ciągu dnia.",
+                pics: [
+                    {
+                        src: zimWes01
+                    },
+                    {
+                        src: zimWes02
+                    },
+                    {
+                        src: zimWes03
+                    },
+                    {
+                        src: zimWes04
+                    },
+                    {
+                        src: zimWes05
+                    },
+                ]
+            },
+            {
+                headerSmall: "Atrakcje weselne - wesele w zimę.",
+                headerClassName: "dark",
+                text: <>Wszystko zależy od klimatu przyjęcia. Za każdym razem, kiedy organizujemy zimowe przyjęcie, staramy się wymyślić coś innego. Coś, co zaskoczy i zaangażuje gości. Dzięki temu na naszych zimowych weselach pojawił się już stół z zimowymi herbatami, wzbogacony o konfitury oraz inne dodatki. Stół z ciepłym kakao oraz ciasteczkami korzennymi i piankami marshmallow, grzaniec i ognisko na zewnątrz, kulig, występ regionalnej kapeli góralskiej. Podczas wesela Marty i Michała postawiliśmy na klasykę. Para młoda, zdecydowała się na obsługę barmańską, ale nie byle jaką. Nasze wesele uświetniło dwóch wspaniałych barmanów z firmy <CustomOuterLink text="Vindebar" to="https://vindebar.pl" className="linkLight" />. Menu było skomponowane specjalnie pod zimowe wesele, a sam bar zachwycał wyglądem i świetnie wpisywał się w dekoracje, ponieważ jego front zdobiło lustro. </>,
+                pics: [
+                    {
+                        src: zimWes06
+                    },
+                    {
+                        src: zimWes07
+                    },
+                    {
+                        src: zimWes08
+                    },
+                    {
+                        src: zimWes09
+                    },
+                    {
+                        src: zimWes10
+                    },
+                ]
+            },
+            {
+                headerSmall: "Zimowy ślub i wesele zorganizuje Czary Marry",
+                headerClassName: "dark",
+                text: "Jeśli marzy Wam się zimowe wesele, zapraszamy do kontaktu. Chętnie porozmawiamy o Waszej wizji i wyczarujemy wesele szyte na miarę Waszych oczekiwań. Mamy duże doświadczenie w organizacji nieszablonowych przyjęć i chętnie pomożemy Wam zrealizować Wasze najskrytsze i nawet najbardziej szalone pomysły.",
+                pics: [
+                    {
+                        src: zimWes11
+                    },
+                    {
+                        src: zimWes12
+                    },
+                    {
+                        src: zimWes13
+                    },
+                    {
+                        src: zimWes14
+                    },
+                    {
+                        src: zimWes15
+                    },
+                ]
+            },
+        ],
+        extras: <>Zobacz również:
+            <ul style={{ marginTop: ".5rem" }}>
+                <li><CustomPostLink text="Ślub i wesele w górach - organizacja + lista miejsc" path="slub-wesele-w-gorach" className="linkLight" /></li>
+                <li><CustomRealisationLink text="Jesienne wesele" path="slub-jesienia-wesele" className="linkLight" /></li>
+                <li><CustomRealisationLink text="Wesele Ani i Mateusza w Uroczysku Siedmiu Stawów" path="uroczysko-siedmiu-stawow-wesele" className="linkLight" /></li>
+            </ul>
+        </>
+    },
     {
         bgColor: "bgPink",
         title: "Wesele Ani i Mateusza w Uroczysku Siedmiu Stawów",
