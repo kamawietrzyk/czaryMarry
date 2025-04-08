954ede663ed43d5888fd219ba4ac906ae1923b78
Kama <kama@wietrzyk.com>
2025-04-08 18:05:31 +0200

    post 36 + fotki

---
+++ src/utils/BlogPics/Post36/index.js
@@ -0,0 +1,35 @@
+import hotelLesDw from './Hotel Leśny Dwór.jpg'
+import hotelLesDw1 from './Hotel Leśny Dwór 1.jpg'
+import hotelLesDw2 from './Hotel Leśny Dwór 2.jpg'
+import hotelLesDw3 from './Hotel Leśny Dwór 3.jpg'
+import lesDwMenu1 from './Leśny Dwór menu 1.jpg'
+import lesDwMenu2 from './Leśny Dwór menu 2.jpg'
+import lesDwMenu3 from './Leśny Dwór menu 3.jpg'
+import lesDwSul1 from './Leśny dwór sulęczyno 1.jpg'
+import lesDwSul2 from './Leśny dwór sulęczyno 2.jpg'
+import lesDwSul3 from './Leśny dwór sulęczyno 3.jpg'
+import lesDwWSul1 from './Leśny Dwór w Sulęczynie 1.jpg'
+import lesDwWSul2 from './Leśny Dwór w Sulęczynie 2.jpg'
+import wesNadJez1 from './Wesele nad jeziorem 1.jpg'
+import wesNadJez2 from './Wesele nad jeziorem 2.jpg'
+import wesNadJez3 from './Wesele nad jeziorem 3.jpg'
+import wesNadJez4 from './Wesele nad jeziorem 4.jpg'
+
+export {
+    hotelLesDw,
+    hotelLesDw1,
+    hotelLesDw2,
+    hotelLesDw3,
+    lesDwMenu1,
+    lesDwMenu2,
+    lesDwMenu3,
+    lesDwSul1,
+    lesDwSul2,
+    lesDwSul3,
+    lesDwWSul1,
+    lesDwWSul2,
+    wesNadJez1,
+    wesNadJez2,
+    wesNadJez3,
+    wesNadJez4
+}
